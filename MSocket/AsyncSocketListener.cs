2c3aa75353f536e5333223ee2a2b1180493d6fc4
Konstantin Balchikov <balchikov@outlook.com>
2013-09-14 20:43:07 +0600

    first commit

---
+++ MSocket/AsyncSocketListener.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MSocket
+{
+    public class AsyncSocketListener
+    {
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
+        public static readonly int Port = 11000;
+
+        public AsyncSocketListener() { }
+
+        public static void StartListening()
+        {
+            // Data buffer for incoming data.
+            byte[] buffer = new byte[1024];
+
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, Port);
+
+            Socket listener = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(backlog:100);
+
+
+                while (true)
+                {
+                    // Set the event to nonsignaled state.
+                    allDone.Reset();
+
+                    // Start an asynchrounous socket to listen for connections.
+                    Console.WriteLine("Waiting for a connection...");
+                    listener.BeginAccept(
+                        new AsyncCallback(AcceptCallback), listener);
+
+                    // Wait until a connection is made before continuing.
+                    allDone.WaitOne();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\nPress ENTER to continue...");
+            Console.Read();
+        }
+
+        private static void AcceptCallback(IAsyncResult ar)
+        {
+            // Signal the main thread to continue.
+            allDone.Set();
+        }
+    }
+}
